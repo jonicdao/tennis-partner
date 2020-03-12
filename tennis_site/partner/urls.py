bafb55bcaee705c4412ff2c99627b921ed6c038f
Jose Nicdao <jonicdao@gmail.com>
2020-03-12 06:47:33 -0700

    modified index and results

--- tennis_site/partner/urls.py
+++ tennis_site/partner/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'partner'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
