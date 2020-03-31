0e3b93a24fb539e33f2de7235f23f82ea1fa3e31
Jose Nicdao <jonicdao@gmail.com>
2020-03-31 15:08:29 -0700

    modified maps

--- tennis_site/partner/static/partner/js/script.js
+++
@@ -1,9 +0,0 @@
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(37.3382,-121.8863),
-  zoom:12,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-
-myMap();
