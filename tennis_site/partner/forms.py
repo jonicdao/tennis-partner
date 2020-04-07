4f42fcfafbda04daf4451993cd1ca0633f084062
Jose Nicdao <jonicdao@gmail.com>
2020-04-06 20:52:45 -0700

    modified forms

--- tennis_site/partner/forms.py
+++ tennis_site/partner/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm
 from .models import Partner
 
 class PostForm(forms.ModelForm):
--- tennis_site/partner/models.py
+++ tennis_site/partner/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+LEVEL_CHOICES = [
+    ('1.0', '1.5', '2.0', '2.5', '3.0', '3.5', '4.0', '4.5', '5.0', '5.5', '6.0')
+]
+
 class Partner(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    level = models.CharField(max_length=5)
+    level = models.CharField(max_length=5, choices=LEVEL_CHOICES)
     zipcode = models.IntegerField()
     email = models.EmailField(max_length=254)
     description = models.TextField(default = "I'm a great player")
